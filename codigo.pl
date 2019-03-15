65b8ad0fad10e317df5b45070e4efb9a2587cdc5
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-03-15 12:24:20 +0100

    Update codigo.pl

--- codigo.pl
+++ codigo.pl
@@ -1,6 +1,6 @@
 % Autores:
 % alumno_prode(Apellido2,Apellido1,Nombre,NumMatricula)
 alumno_prode('Nieves','Sanchez','Victor','X150375').
-alumno_prode('Carmona','Ayllon',Alejandro','X150251').
+alumno_prode('Carmona','Ayllon','Alejandro','X150251').
 alumno_prode('Morgera','Perez','Daniel','X150284').
 

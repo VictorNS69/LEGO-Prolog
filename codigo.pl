9c48dff19605b89e609047b00f31447f2edbdc93
Víctor Nieves Sánchez <vnievess@gmail.com>
2019-03-28 11:29:22 +0100

    fixed errata in lowerEq

--- codigo.pl
+++ codigo.pl
@@ -4,14 +4,15 @@ alumno_prode('Nieves','Sanchez','Victor','X150375').
 alumno_prode('Carmona','Ayllon','Alejandro','X150251').
 alumno_prode('Morgera','Perez','Daniel','X150284').
 
-% Programa:
-% lowerEq(X,Y) --> X <= Y
+% ---------------------------------------------------------
+
+% lowerEq/2 Dice si X es menor o igual que Y
 lowerEq(X,X).
 lowerEq(0,X) :- X\=0.		% min(0,X) = X
 lowerEq(s(X),s(Y)) :-
-	lower(X,Y).
+	lowerEq(X,Y).
 
-% compPiezas/2
+% compPiezas/2 Compara si la pieza1 es menor que la pieza2 en anchura y profundidad
 compPiezas(pieza(AN1,_,P1,_),pieza(AN2,_,P2,_)):-
-	lower(AN1,AN2),
-	lower(P1,P2).
+	lowerEq(AN1,AN2),
+	lowerEq(P1,P2).

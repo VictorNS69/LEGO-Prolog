2ac02940981af24c7a36d0d49af4240db2d68f02
dmorgera <daniel.morgera97@gmail.com>
2019-03-28 11:23:23 +0100

    Created auxiliary functions

--- codigo.pl
+++ codigo.pl
@@ -4,3 +4,14 @@ alumno_prode('Nieves','Sanchez','Victor','X150375').
 alumno_prode('Carmona','Ayllon','Alejandro','X150251').
 alumno_prode('Morgera','Perez','Daniel','X150284').
 
+% Programa:
+% lowerEq(X,Y) --> X <= Y
+lowerEq(X,X).
+lowerEq(0,X) :- X\=0.		% min(0,X) = X
+lowerEq(s(X),s(Y)) :-
+	lower(X,Y).
+
+% compPiezas/2
+compPiezas(pieza(AN1,_,P1,_),pieza(AN2,_,P2,_)):-
+	lower(AN1,AN2),
+	lower(P1,P2).
